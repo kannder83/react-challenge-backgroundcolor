9a61e5e5f64d90693222a47792b5d7250931d959
Alejandro Ruiz <kannder.ziur@gmail.com>
2021-08-16 12:43:50 -0500

    I add new elements for the challenger.

---
+++ src/components/ShowColorBackground.js
@@ -0,0 +1,9 @@
+const ShowColorBackground = () => {
+  return (
+    <div className="content">
+      <div className="box-color">Color here</div>
+    </div>
+  );
+};
+
+export default ShowColorBackground;
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <header>
+      <h1 className="header_title">Find color name</h1>
+    </header>
+  );
+};
+
+export default Title;
